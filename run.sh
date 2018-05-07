6da00a192ab59943993e15b5d45df49bea0256b1
Leon <leondevlifelog@gmail.com>
2018-05-07 19:36:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 file="~/.ssh/id_rsa"
 if [ ! -f "$file" ]; then
-	sudo mkdir -p ~/.ssh 
-	sudo ssh-keygen -q -t rsa -N 'gerrit' -f ~/.ssh/id_rsa
-	sudo chown gerrit: -R ~/.ssh
+	mkdir -p ~/.ssh 
+	ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
 fi
 echo "====================================ssh Public key======================================================"
 cat ~/.ssh/id_rsa.pub
 echo "====================================ssh Public key======================================================"
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
+ssh-keyscan -t -p 10022 rsa git.xyz.cn > ~/.ssh/known_hosts
 git config -f /var/gerrit/etc/gerrit.config gerrit.canonicalWebUrl "${CANONICAL_WEB_URL:-http://$HOSTNAME:8080/}" && \
 git config -f /var/gerrit/etc/gerrit.config noteDb.changes.autoMigrate true && \
 /var/gerrit/bin/gerrit.sh run
